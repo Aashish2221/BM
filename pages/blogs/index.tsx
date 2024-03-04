50d0116ac747e76db740b337382894289968fc03
Manjusha Karpe <patilaashish2221@gmail.com>
2024-03-04 18:08:19 +0530

    new_blog_change

--- pages/blogs/index.tsx
+++ pages/blogs/index.tsx
@@ -1,4 +1,3 @@
-import ShareModal from '@/components/ModalForm/ShareModal/shareModal';
 import useToggle from '@/hooks/useToggle';
 import {
   Card,
@@ -8,16 +7,14 @@ import {
 } from '@material-tailwind/react';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BsArrowRight } from 'react-icons/bs';
 import Head from 'next/head';
 import data from '@/data';
-import Spinner from '@/components/Spinner';
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import { getBlogData } from '@/services/spot-prices';
-import { Blog } from '@/interfaces/typeinterfaces';
-import { link } from 'fs';
-
+const ShareModal = React.lazy(()=>import('@/components/ModalForm/ShareModal/shareModal'))
+const Spinner = React.lazy(()=>import('@/components/Spinner'))
 export default function Blogs({
   title,
   blogs
@@ -45,7 +42,7 @@ export default function Blogs({
         <meta property='og:url' content={canonicalUrl} key={canonicalUrl} />
         <link rel='canonical' href={canonicalUrl} />
         {
-          blogs.map((blogs:Blog)=>(
+          blogs.map((blogs:any)=>(
             <><link rel="preload" as='image' href={blogs.image} /></>
           ))
         }
@@ -59,7 +56,7 @@ export default function Blogs({
           </h1>
           {/* ----------------- blog section ------------- */}
           <section className='container mx-auto mt-14 grid grid-cols-12 gap-4 sm:mt-20 lg:mt-24 xl:mt-24 2xl:mt-28'>
-            {blogs.map((blogs: Blog) => (
+            {blogs.map((blogs: any) => (
               <Card
                 key={blogs.id}
                 className='col-span-12 mx-auto mt-6 mb-10 w-full duration-300 hover:-translate-y-1 hover:scale-105 hover:shadow-md sm:col-span-6 sm:mb-20 sm:mt-6 sm:h-[23rem]
@@ -78,15 +75,14 @@ export default function Blogs({
                     <Image
                       src={blogs.image}
                       alt={blogs.title}
-
                       className='h-40 w-full rounded-[17px] px-1 sm:h-44 md:h-48 lg:h-48 xl:h-52'
                       height={400} 
                       width={400}
-                      priority
+                      loading='eager'
                     />
                   </CardHeader>
 
-                  <CardBody className='mt-0 px-4 pt-2 sm:pt-3 md:mt-3 md:pt-2 lg:-mt-2 xl:mt-1'>
+                  <CardBody className='px-4 pt-2'>
                     <h3 className='text-[1.125rem] font-semibold leading-5 h-10'>
                       {blogs.title}
                     </h3>
@@ -100,7 +96,7 @@ export default function Blogs({
                             : blogs.description.slice(0, 175) + '...'
                       }}
                     ></p>
-                    <h4 className='pt-24  text-xs font-normal italic text-[#5c5b5b] md:pt-20 lg:pt-24 2xl:pt-[4.5rem]'>
+                    <h4 className='pt-24  text-xs font-normal italic text-[#5c5b5b]  2xl:pt-[4.5rem]'>
                       By BullionMentor on{' '}
                       {new Intl.DateTimeFormat('en-US', {
                         month: 'long',
@@ -109,11 +105,11 @@ export default function Blogs({
                       }).format(new Date(blogs.publishdate))}
                     </h4>
                   </CardBody>
-                  <CardFooter className='mt-0 flex sm:mt-1 xl:mt-1 2xl:mt-2'>
+                  <CardFooter className='sm:mt-2'>
                     <Link
                       href={`/blogs/${blogs.code}`}
                       as={`/blogs/${blogs.code}`}
-                      className='flex px-4 py-2 font-semibold text-primary shadow-none hover:underline hover:underline-offset-2 md:px-6 md:py-2 md:text-sm lg:px-4 lg:py-1 lg:text-sm'
+                      className='flex px-4 font-semibold text-primary shadow-none hover:underline hover:underline-offset-2 md:px-6 md:py-2 md:text-sm lg:px-4 lg:py-1 lg:text-sm'
                       passHref
                       prefetch={false}
                     >
