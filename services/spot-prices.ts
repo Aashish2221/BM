1a9c8416c6d0c994fe238708553b7d3ab0ba1ccb
Manjusha Karpe <patilaashish2221@gmail.com>
2024-03-05 17:03:44 +0530

    spinner_5

--- services/spot-prices.ts
+++ services/spot-prices.ts
@@ -75,12 +75,9 @@ export const getDealers = async () => {
   );
   return res.data.data;
 };
-<<<<<<< Updated upstream
 
 // Blog
 
-=======
->>>>>>> Stashed changes
 export const getBlogData = async (pageSize: number, pageNumber: number) => {
   const res = await fetcher.get<ApiResponse<Blog[]>>(
     `${process.env.BASE_URL}/api/BestBullionDeals/GetBlogs?size=${pageSize}&pagenumber=${pageNumber}`
