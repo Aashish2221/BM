6118010f217c7db185ba356abb384140a635e415
Manjusha Karpe <patilaashish2221@gmail.com>
2024-03-06 16:20:43 +0530

    change_api

--- next.config.js
+++ next.config.js
@@ -313,7 +313,7 @@ const nextConfig = {
     unoptimized: true
   },
   env: {
-    BASE_URL: 'http://106.201.231.27:8082',
+    BASE_URL: 'https://bbdapi.bestbulliondeals.com',
     WEBSITE_URL: 'https://www.bullionmentor.com',
     NEXTAUTH_URL: 'https://www.bullionmentor.com'
   }
