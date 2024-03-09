7465e67910abe7b99dd8f4d0de6dbe13ab51b57f
Aashish@2221 <aashishpatil2221@gmail.com>
2024-03-09 12:28:47 +0530

    devMain

--- pages/blogs/[code].tsx
+++ pages/blogs/[code].tsx
@@ -60,7 +60,7 @@ const Blog = ({
   } else {
     return (
       <>
-        <Head>
+        <Head children={undefined}>
           <title>{title}</title>
           <meta property='og:url' content={canonicalUrl} key={canonicalUrl} />
           <link rel='canonical' href={canonicalUrl} />
