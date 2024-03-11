2bbba381f22f8cec6c24e992f8037ff251d291f1
Aashish@2221 <aashishpatil2221@gmail.com>
2024-03-11 13:26:15 +0530

    new-all

--- pages/blogs/index.tsx
+++ pages/blogs/index.tsx
@@ -45,7 +45,7 @@ export default function Blogs({
           content={data.WEBSITEUrl + '/blogs'}
           key={data.WEBSITEUrl + '/blogs'}
         />
-        <Link rel='canonical' href={data.WEBSITEUrl + '/blogs'} />
+        <link rel='canonical' href={data.WEBSITEUrl + '/blogs'} />
         {
           blogs.map((blog:any)=>(
            <Link key={blog.id} rel="preload" as="image" href={blog.image} />
@@ -63,42 +63,40 @@ export default function Blogs({
           loader={<SpinnerBlog />}
         >
           <section className='container mx-auto mt-14 grid grid-cols-12 gap-4 sm:mt-20 lg:mt-24 2xl:mt-28'>
-            {blogs.map((blog:any ) => (
+            {blogs.map((blogs:any ) => (
               <Card
-                key={blog.id}
+                key={blogs.id}
                 className='col-span-12 mx-auto mt-6 mb-10 h-[22rem] w-full duration-300 hover:-translate-y-1 hover:scale-105 hover:shadow-md sm:col-span-6 sm:mb-20 sm:mt-6 sm:h-[23rem]
                    lg:col-span-4 lg:mb-20 lg:mt-2 lg:h-96 2xl:col-span-3 2xl:h-[22rem]'
               >
                 <Link
-                  href={`/blogs/${blog.code}`}
-                  as={`/blogs/${blog.code}`}
+                  href={`/blogs/${blogs.code}`}
+                  as={`/blogs/${blogs.code}`}
                   passHref
                   prefetch={false}
                 >
                   <CardHeader
                     floated={true}
-                    className='mx-1 -mt-16 h-40 shadow-none sm:mt-[-4rem] sm:h-44 md:-mt-20 md:h-48 lg:-mt-[65px] lg:h-52 xl:mx-2 xl:-mt-20'
+                    className='mx-2 rounded-[17px] -mt-16 h-40 shadow-none sm:mt-[-4rem] sm:h-44 md:-mt-20 md:h-48 lg:-mt-[65px] lg:h-52 xl:mx-2 xl:-mt-20'
                   >
                     {' '}
                     <img
-
-                      src={blog.image}
-                      alt={blog.title}
+                      src={blogs.image}
+                      alt={blogs.title} 
                       loading='eager'
-
                      />
                   </CardHeader>
                   <CardBody className='px-4 pt-2 sm:pt-3 md:mt-3  lg:-mt-2 xl:mt-1'>
                     <h3 className='h-10 text-[1.125rem] font-semibold leading-5 md:h-9'>
-                      {blog.title}
+                      {blogs.title}
                     </h3>
                     <p
                       className='h-10 pt-6 text-[0.95rem] leading-[1.4rem] text-gray-500'
                       dangerouslySetInnerHTML={{
                         __html:
-                          blog.shortDescription.length <= 29
-                            ? blog.shortDescription
-                            : blog.shortDescription.slice(0, 100) + '...'
+                          blogs.shortDescription.length <= 29
+                            ? blogs.shortDescription
+                            : blogs.shortDescription.slice(0, 100) + '...'
                       }}
                     ></p>
                     <h4 className='pt-24  text-xs font-normal italic text-[#5c5b5b] md:pt-20 lg:pt-24 2xl:pt-[4.5rem]'>
@@ -107,13 +105,13 @@ export default function Blogs({
                         month: 'long',
                         day: 'numeric',
                         year: 'numeric'
-                      }).format(new Date(blog.publishdate))}
+                      }).format(new Date(blogs.publishdate))}
                     </h4>
                   </CardBody>
                   <CardFooter className='mt-0 flex sm:mt-1 xl:mt-1 2xl:mt-2'>
                     <Link
-                      href={`/blogs/${blog.code}`}
-                      as={`/blogs/${blog.code}`}
+                      href={`/blogs/${blogs.code}`}
+                      as={`/blogs/${blogs.code}`}
                       className='flex px-4 py-2 font-semibold text-primary shadow-none hover:underline hover:underline-offset-2 md:px-6 md:py-2 md:text-sm lg:px-4 lg:py-1 lg:text-sm'
                       passHref
                       prefetch={false}
@@ -143,14 +141,15 @@ export default function Blogs({
   );
 }
 
-export const getServerSideProps: GetServerSideProps<{
-  blogs: Awaited<ReturnType<typeof getBlogData>>;
-}> = async ({ res }) => {
-  res.setHeader('Cache-control', 'public, sa-maxage=10, state-while-revalidate=59');
-  const blogs = await getBlogData(1, 8);
-  const blog = data.site.blog;
-  const title = blog.page
-  const description = blog.description
-  return { props: {title ,description, blogs}
-  };
-};
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+    res.setHeader( 'Cache-Control', 'public, s-maxage=10, stale-while-revalidate=59' );
+    const pageNumber = 1;
+    const initialBlogs = await getBlogData(pageSize, pageNumber);
+    const blog = data.site.blog;
+    const title = blog.page;
+    const description = blog.description;
+    return {
+      props: {title, description, initialBlogs}
+    }
+  }
+  
