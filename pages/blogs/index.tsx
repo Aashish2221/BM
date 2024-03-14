b9ffd72ab4e2e0120cf9b175d2077e3ae2618360
Aashish@2221 <aashishpatil2221@gmail.com>
2024-03-14 11:40:54 +0530

    new-0

--- pages/blogs/index.tsx
+++ pages/blogs/index.tsx
@@ -1,27 +1,22 @@
-
-import { Card, CardHeader, CardBody, CardFooter } from '@material-tailwind/react';
-import Image from 'next/image';
-import Link from 'next/link';
-import { useEffect, useState, useMemo } from 'react';
-import { BsArrowRight } from 'react-icons/bs';
-import Head from 'next/head';
-import data from '@/data';
+import { useState } from 'react';
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
+import Head from 'next/head';
 import { getBlogData } from '@/services/spot-prices';
 import InfiniteScroll from 'react-infinite-scroll-component';
-import dynamic from 'next/dynamic';
-const BlogIndexSkeleton= dynamic(()=>import('@/components/Loaders/Blogs/BlogIndexSkeleton'));
-const SpinnerBlog = dynamic(()=>import('@/components/Spinner'))
+import { BlogCard } from '@/components/BlogCard';
+import BlogIndexSkeleton from '@/components/Loaders/Blogs/BlogIndexSkeleton';
+import { SpinnerBlog } from '@/components/Spinner';
+import data from '@/data';
+
 const pageSize = 8;
 
 export default function Blogs({
   title,
   initialBlogs,
 }: InferGetServerSidePropsType<typeof getServerSideProps> | any) {
-  const [blogs, setBlogs] = useState<any>(initialBlogs);
+  const [blogs, setBlogs] = useState(initialBlogs);
   const [page, setPage] = useState(1);
   const [hasMore, setHasMore] = useState(true);
-
 
   const loadMoreBlogs = async () => {
     try {
@@ -38,99 +33,26 @@ export default function Blogs({
     }
   };
 
-  const canonicalUrl = data.WEBSITEUrl + '/blogs';
-  const memoizedBlogs = useMemo(() => blogs, [blogs]);
-
   return (
     <>
       <Head>
         <title>{title}</title>
-        <meta property="og:url" content={canonicalUrl} key={canonicalUrl} />
-        <link rel="canonical" href={canonicalUrl} />
-        {memoizedBlogs.map((blog: any) => (
-          <link key={blog.id} rel="preload" as="image" href={blog.image} />
-        ))}
       </Head>
-      {memoizedBlogs.length > 0 ? (
+      {blogs.length > 0 ? (
         <div className="text-dark-black">
-          <h1 className="container mx-auto mt-14 text-xl font-semibold md:mt-16 md:text-2xl lg:mt-5">
-            Blog
-          </h1>
+          <h1 className="container mx-auto mt-14 text-xl font-semibold md:mt-16 md:text-2xl lg:mt-5">Blog</h1>
           <InfiniteScroll
-            dataLength={memoizedBlogs.length}
+            dataLength={blogs.length}
             next={loadMoreBlogs}
             hasMore={hasMore}
             loader={<SpinnerBlog />}
           >
             <section className="container mx-auto mt-14 grid grid-cols-12 gap-4 sm:mt-20 lg:mt-24 xl:mt-24 2xl:mt-28">
-              {memoizedBlogs.map((blog: any) => (
-                <Card
-                  key={blog.id}
-                  className="col-span-12 mx-auto mt-6 mb-10 h-[22rem] w-full duration-300 hover:-translate-y-1 hover:scale-105 hover:shadow-md sm:col-span-6 sm:mb-20 sm:mt-6 sm:h-[23rem] lg:col-span-4 lg:mb-20 lg:mt-2 lg:h-96 2xl:col-span-3 2xl:h-[22rem]"
-                >
-                  <Link
-                    href={`/blogs/${blog.code}`}
-                    as={`/blogs/${blog.code}`}
-                    passHref
-                    prefetch={false}
-                  >
-                    <CardHeader
-                      floated={true}
-                      className="mx-1 -mt-16 h-40 shadow-none sm:mt-[-4rem] sm:h-44 md:-mt-20 md:h-48 lg:-mt-[65px] lg:h-52 xl:mx-2 xl:-mt-20"
-                    >
-                      <Image
-                        src={blog.image}
-                        alt={blog.title}
-                        height={200}
-                        width={350}
-                        className="h-40 w-full rounded-[17px] px-1 sm:h-44 lg:h-48 xl:h-52"
-                        loading="eager"
-                        priority
-                      />
-                    </CardHeader>
-                    <CardBody className="px-4 pt-2 sm:pt-3 md:mt-3 md:pt-2 lg:-mt-2 xl:mt-1">
-                      <h3 className="h-10 text-[1.125rem] font-semibold leading-5 md:h-9">
-                        {blog.title}
-                      </h3>
-                      <p
-                        className="h-10 pt-6 text-[0.95rem] leading-[1.4rem] text-gray-500"
-                        dangerouslySetInnerHTML={{
-                          __html:
-                            blog.shortDescription.length <= 29
-                              ? blog.shortDescription
-                              : blog.shortDescription.slice(0, 100) + '...',
-                        }}
-                      ></p>
-                      <h4 className="pt-24 text-xs font-normal italic text-[#5c5b5b] md:pt-20 lg:pt-24 2xl:pt-[4.5rem]">
-                        By BullionMentor on{' '}
-                        {new Intl.DateTimeFormat('en-US', {
-                          month: 'long',
-                          day: 'numeric',
-                          year: 'numeric',
-                        }).format(new Date(blog.publishdate))}
-                      </h4>
-                    </CardBody>
-                    <CardFooter className="flex sm:mt-1 2xl:mt-2">
-                      <Link
-                        href={`/blogs/${blog.code}`}
-                        as={`/blogs/${blog.code}`}
-                        className="flex px-4 py-2 font-semibold text-primary shadow-none hover:underline hover:underline-offset-2 md:px-6 md:text-sm lg:px-4 lg:py-1 lg:text-sm"
-                        passHref
-                        prefetch={false}
-                      >
-                        Read More
-                        <BsArrowRight
-                          className="ml-1 text-primary"
-                          size={20}
-                        ></BsArrowRight>
-                      </Link>
-                    </CardFooter>
-                  </Link>
-                </Card>
+              {blogs.map((blog: any) => (
+                <BlogCard key={blog.id} blog={blog} />
               ))}
             </section>
           </InfiniteScroll>
-          {/* ShareModal goes here */}
         </div>
       ) : (
         <BlogIndexSkeleton />
