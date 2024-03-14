1b41fe7c68078549791c06d7d793ea7ee399fb0a
Aashish@2221 <aashishpatil2221@gmail.com>
2024-03-14 12:53:26 +0530

    Update index.tsx

--- pages/blogs/index.tsx
+++ pages/blogs/index.tsx
@@ -40,7 +40,7 @@ export default function Blogs({
           <link key={blog.id} rel='preload' as='image' href={blog.image} />
         ))}
       </Head>
-      <Suspense fallback={<DashboardSkeleton/>}>
+      <Suspense fallback={<BlogIndexSkeleton/>}>
       {blogs.length > 0 ? (
         <div className="text-dark-black">
           <h1 className="container mx-auto mt-14 text-xl font-semibold md:mt-16 md:text-2xl lg:mt-5">Blog</h1>
