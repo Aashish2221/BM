0fa7889e7ec9492ee41a9a452a89c94970b14d99
Aashish@2221 <aashishpatil2221@gmail.com>
2024-03-15 13:29:41 +0530

    d

--- components/Loaders/BlogIndexSkeleton/BlogSkeleton.tsx
+++ components/Loaders/BlogIndexSkeleton/BlogSkeleton.tsx
@@ -1,3 +1,4 @@
+import { Card, CardBody, CardFooter } from '@material-tailwind/react';
 import { useState } from 'react';
 
 const BlogSkeleton = () => {
@@ -9,21 +10,21 @@ const BlogSkeleton = () => {
       </h1>
       <section className='container mx-auto mt-14 grid grid-cols-12 gap-4 sm:mt-20 lg:mt-24 xl:mt-24 2xl:mt-28'>
         {blogs.map((blog: any) => (
-          <div key={blog.id} className='col-span-12 mx-auto mt-6 mb-10 h-[22rem] w-full duration-300 hover:-translate-y-1 hover:scale-105 hover:shadow-md sm:col-span-6 sm:mb-20 sm:mt-6 sm:h-[23rem] lg:col-span-4 lg:mb-20 lg:mt-2 lg:h-96 2xl:col-span-3 2xl:h-[22rem]'>
+          <Card key={blog.id} className='col-span-12 mx-auto mt-6 mb-10 h-[22rem] w-full duration-300 hover:-translate-y-1 hover:scale-105 hover:shadow-md sm:col-span-6 sm:mb-20 sm:mt-6 sm:h-[23rem] lg:col-span-4 lg:mb-20 lg:mt-2 lg:h-96 2xl:col-span-3 2xl:h-[22rem]'>
             <div className='animate-pulse'>
               <div className='h-40 rounded-t-lg bg-gray-200'></div>
-              <div className='px-4 pt-2 sm:pt-3 md:mt-3 md:pt-2 lg:-mt-2 xl:mt-1'>
+              <CardBody className='px-4 pt-2 sm:pt-3 md:mt-3 md:pt-2 lg:-mt-2 xl:mt-1'>
                 <h3 className='mb-2 h-10 w-3/4 bg-gray-200'></h3>
                 <p className='mb-4 h-10 w-full bg-gray-200'></p>
                 <p className='h-6 w-1/2 bg-gray-200'></p>
-              </div>
-              <div className='flex sm:mt-1 2xl:mt-2'>
+              </CardBody>
+              <CardFooter className='flex sm:mt-1 2xl:mt-2'>
                 <div className='flex w-full justify-center bg-gray-200 px-4 py-2 font-semibold text-primary'>
                   <div className='w-4/5'></div>
                 </div>
-              </div>
+              </CardFooter>
             </div>
-          </div>
+          </Card>
         ))}
       </section>
     </div>
--- pages/blogs/index.tsx
+++ pages/blogs/index.tsx
@@ -5,22 +5,31 @@ import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import { getBlogsData } from '@/services/spot-prices';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import dynamic from 'next/dynamic';
-import { useMemo, useState } from 'react';
-// import BlogSkeleton from '@/components/Loaders/BlogIndexSkeleton/BlogSkeleton';
-const SpinnerBlog = dynamic(()=>import('@/components/Spinner'))
+import { Suspense, useEffect, useMemo, useState } from 'react';
+import SpinnerBlog from '@/components/Spinner';
+import Link from 'next/link';
 const BlogSkeleton = dynamic(()=>import('@/components/Loaders/BlogIndexSkeleton/BlogSkeleton'))
 const ShareModal = dynamic(()=>import('@/components/ModalForm/ShareModal/shareModal'))
 const BlogCard = dynamic(()=>import('@/components/BlogCard'))
 const pageSize = 8;
 export default function Blogs({
-  title ,initialBlogs
+  title ,
 }: InferGetServerSidePropsType<typeof getServerSideProps> | any) {
   const [shareModal, toggleShareModal] = useToggle();
   const [share, setShare] = useState<any>(window.location.href);
-  const [blogs, setBlogs] = useState<any[]>(initialBlogs);
+  const [blogs, setBlogs] = useState<any[]>([]);
   const [page, setPage] = useState(0);
   const [hasMore, setHasMore] = useState(true);
- 
+  const [hydrate, setHydrated]= useState(true)
+  useEffect(()=>{
+     const data = async()=>{
+      const pageNumber = 1;
+      const initialBlogs = await getBlogsData(pageSize, pageNumber);
+      setBlogs(initialBlogs);
+      setHydrated(false)
+     }
+     data();
+  },[])
   const loadMoreBlogs = async () => {
     const nextPage = page + 1;
     const newBlogs = await getBlogsData(pageSize, nextPage);
@@ -32,18 +41,18 @@ export default function Blogs({
     }
 };
   const canonicalUrl = data.WEBSITEUrl + '/blogs';
-  const memoizedBlogs = useMemo(() => blogs, [blogs]);
   return (
     <>
       <Head>
         <title>{title}</title>
         <meta property='og:url' content={canonicalUrl} key={canonicalUrl} />
         <link rel='canonical' href={canonicalUrl} />
-        {memoizedBlogs.map((blog:any)=>
-        <link key={blog.id} rel="preload" as='image' href={blog.image} />
+        {blogs.map((blog:any)=>
+        <Link key={blog.id} rel="preload" as='image' href={blog.image} />
         )}
       </Head>
-      {blogs.length > 0 ? 
+      <Suspense fallback={<BlogSkeleton/>}>
+      {hydrate ?  <BlogSkeleton/> :
         <div className='text-dark-black'>
           <h1 className='semibold container mx-auto mt-14 text-xl font-medium md:mt-16 md:text-2xl lg:mt-5'>
             Blog
@@ -56,7 +65,7 @@ export default function Blogs({
           >
           {/* ----------------- blog section ------------- */}
           <section className='container mx-auto mt-14 grid grid-cols-12 gap-4 sm:mt-20 lg:mt-24 xl:mt-24 2xl:mt-28'>
-            {memoizedBlogs.map((blog:any ) => (
+            {blogs.map((blog:any ) => (
               <BlogCard key={blog.id} blog={blog} />
             ))}
           </section>
@@ -70,20 +79,19 @@ export default function Blogs({
             />
           )}
         </div>
-        : <BlogSkeleton/>}
+        }
+        </Suspense>
     </>
   );
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
   res.setHeader( 'Cache-Control', 'public, s-maxage=10, stale-while-revalidate=59' );
-  const pageNumber = 1;
-  const initialBlogs = await getBlogsData(pageSize, pageNumber);
   const blog = data.site.blog;
   const title = blog.page;
   const description = blog.description;
   return {
-    props: {title, description, initialBlogs}
+    props: {title, description}
   }
 }
 
