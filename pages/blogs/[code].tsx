9d8dbdc57f997d07fa1042fc0648e852a31d6c73
Aashish@2221 <aashishpatil2221@gmail.com>
2024-03-19 12:22:37 +0530

    Update [code].tsx

--- pages/blogs/[code].tsx
+++ pages/blogs/[code].tsx
@@ -3,92 +3,69 @@ import Head from 'next/head';
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import { getBlogDetails } from '@/services/spot-prices';
 import data from '@/data';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { BlogSideCard } from '@/components/BlogDescription';
 import BlogSlugSkeleton from '@/components/Loaders/BlogIndexSkeleton/BlogSlugSkeleton';
-import axios from 'axios';
 const Description = React.lazy(() => import('@/components/BlogDescription'));
 const Blog = ({
   title,
-  description
-}: 
-InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  description,
+  blogData
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const router = useRouter();
   const { code } = router.query;
 
   const formattedPath = router.asPath.replace(`/blogs?.Title = ${code}`, '');
   const canonicalUrl = data.WEBSITEUrl + formattedPath;
-  const [loading, setLoading] = useState(true);
-  const [blogData, setBlogData] = useState<any>();
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await axios.get(
-          `${
-            process.env.BASE_URL
-          }/api/BestBullionDeals/GetBlogDetails?Title=${code?.toString()}`
-        );
-        setBlogData(res.data.data);
-      } catch (error) {
-        console.error('Error fetching blog data:', error);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchData();
-  }, [code]);
   return (
     <>
-      {loading ? (
+      <Head>
+        <title>{title}</title>
+        <meta property='og:url' content={canonicalUrl} key={canonicalUrl} />
+        <link rel='canonical' href={canonicalUrl} />
+      </Head>
+      {blogData.description.length === 0 ? (
         <BlogSlugSkeleton />
       ) : (
-        <>
-          <Head>
-            <title>{title}</title>
-            <meta property='og:url' content={canonicalUrl} key={canonicalUrl} />
-            <link rel='canonical' href={canonicalUrl} />
-          </Head>
-          <div className='grid-col container mx-auto'>
-            <div className='mx-auto mt-16 grid max-w-[1400px] grid-cols-12 gap-0 text-dark-black sm:container sm:gap-4 md:mt-10'>
-              <div className='col-span-12 md:col-span-8'>
-                <span className='lg:grid-col gap-1 lg:grid'>
-                  <Images blogData={blogData} />
-                  {/* ------ heading ------- */}
-                  <header className='pt-5 text-lg font-semibold text-primary md:text-2xl md:font-medium'>
-                    <h1>{blogData?.title}</h1>
-                  </header>
-                  <section className='pt-4 text-xs font-bold italic text-[#5c5b5b]'>
-                    <h6>
-                      By BullionMentor on{' '}
-                      {new Intl.DateTimeFormat('en-US', {
-                        month: 'long',
-                        day: 'numeric',
-                        year: 'numeric'
-                      }).format(new Date(blogData.publishdate))}
-                    </h6>
-                  </section>
-                  {/* ----- sub-heading and paragraph ----- */}
-                  {<Description blogData={blogData} />}
+        <div className='grid-col container mx-auto'>
+          <div className='mx-auto mt-16 grid max-w-[1400px] grid-cols-12 gap-0 text-dark-black sm:container sm:gap-4 md:mt-10'>
+            <div className='col-span-12 md:col-span-8'>
+              <span className='lg:grid-col gap-1 lg:grid'>
+                <Images blogData={blogData} />
+                {/* ------ heading ------- */}
+                <header className='pt-5 text-lg font-semibold text-primary md:text-2xl md:font-medium'>
+                  <h1>{blogData?.title}</h1>
+                </header>
+                <section className='pt-4 text-xs font-bold italic text-[#5c5b5b]'>
+                  <h6>
+                    By BullionMentor on{' '}
+                    {new Intl.DateTimeFormat('en-US', {
+                      month: 'long',
+                      day: 'numeric',
+                      year: 'numeric'
+                    }).format(new Date(blogData.publishdate))}
+                  </h6>
+                </section>
+                {/* ----- sub-heading and paragraph ----- */}
+                {<Description blogData={blogData} />}
 
-                  {/*-------------------------- Blog Content End --------------------- */}
-                </span>
-              </div>
-              {/* --------------------- Blog Side Card------------------- */}
-              <div className='col-span-12 mt-4 md:col-span-4 md:mt-0'>
-                <div className='container rounded-md pb-4 shadow-md shadow-slate-300'>
-                  <img
-                    src={blogData.image}
-                    alt={blogData.title}
-                    className='rounded-md p-4 lg:w-full'
-                    loading='lazy'
-                  />
-                  <BlogSideCard blogData={blogData} />
-                </div>
+                {/*-------------------------- Blog Content End --------------------- */}
+              </span>
+            </div>
+            {/* --------------------- Blog Side Card------------------- */}
+            <div className='col-span-12 mt-4 md:col-span-4 md:mt-0'>
+              <div className='container rounded-md pb-4 shadow-md shadow-slate-300'>
+                <img
+                  src={blogData.image}
+                  alt={blogData.title}
+                  className='rounded-md p-4 lg:w-full'
+                  loading='lazy'
+                />
+                <BlogSideCard blogData={blogData} />
               </div>
             </div>
           </div>
-        </>
+        </div>
       )}
     </>
   );
@@ -101,7 +78,7 @@ export const getServerSideProps: GetServerSideProps = async (res) => {
   const title = blogData.metatitle;
   const description = blogData.metaDescription;
   return {
-    props: { title, description }
+    props: { title, description, blogData: blogData }
   };
 };
 
