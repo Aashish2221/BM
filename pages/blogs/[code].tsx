06ffd152220cbd5cc6cc4339a80ad193094e9bef
Aashish@2221 <aashishpatil2221@gmail.com>
2024-03-19 13:17:39 +0530

    i

--- pages/blogs/[code].tsx
+++ pages/blogs/[code].tsx
@@ -3,10 +3,8 @@ import Head from 'next/head';
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import { getBlogDetails } from '@/services/spot-prices';
 import data from '@/data';
-import React, { useEffect, useState } from 'react';
-import { BlogSideCard } from '@/components/BlogDescription';
-import BlogSlugSkeleton from '@/components/Loaders/BlogIndexSkeleton/BlogSlugSkeleton';
-const Description = React.lazy(() => import('@/components/BlogDescription'));
+import dynamic from 'next/dynamic';
+const Description = dynamic(() => import('@/components/BlogDescription'));
 const Blog = ({
   title,
   description,
@@ -17,25 +15,54 @@ const Blog = ({
 
   const formattedPath = router.asPath.replace(`/blogs?.Title = ${code}`, '');
   const canonicalUrl = data.WEBSITEUrl + formattedPath;
-  
+
   return (
     <>
       <Head>
         <title>{title}</title>
         <meta property='og:url' content={canonicalUrl} key={canonicalUrl} />
         <link rel='canonical' href={canonicalUrl} />
-        <link rel="preload" as='image' href={blogData.image} />
+        <link rel='preload' as='image' href={blogData.image} />
       </Head>
-        <div className='grid-col container mx-auto'>
-          <div className='mx-auto mt-16 grid max-w-[1400px] grid-cols-12 gap-0 text-dark-black sm:container sm:gap-4 md:mt-10'>
-            <div className='col-span-12 md:col-span-8'>
-              <span className='lg:grid-col gap-1 lg:grid'>
-                <Images blogData={blogData} />
-                {/* ------ heading ------- */}
-                <header className='pt-5 text-lg font-semibold text-primary md:text-2xl md:font-medium'>
-                  <h1>{blogData?.title}</h1>
+      <div className='grid-col container mx-auto'>
+        <div className='mx-auto mt-16 grid max-w-[1400px] grid-cols-12 gap-0 text-dark-black sm:container sm:gap-4 md:mt-10'>
+          <div className='col-span-12 md:col-span-8'>
+            <span className='lg:grid-col gap-1 lg:grid'>
+              <Images blogData={blogData} />
+              {/* ------ heading ------- */}
+              <header className='pt-5 text-lg font-semibold text-primary md:text-2xl md:font-medium'>
+                <h1>{blogData?.title}</h1>
+              </header>
+              <section className='pt-4 text-xs font-bold italic text-[#5c5b5b]'>
+                <h6>
+                  By BullionMentor on{' '}
+                  {new Intl.DateTimeFormat('en-US', {
+                    month: 'long',
+                    day: 'numeric',
+                    year: 'numeric'
+                  }).format(new Date(blogData.publishdate))}
+                </h6>
+              </section>
+              {/* ----- sub-heading and paragraph ----- */}
+              <Description blogData={blogData} />
+
+              {/*-------------------------- Blog Content End --------------------- */}
+            </span>
+          </div>
+          {/* --------------------- Blog Side Card------------------- */}
+          <div className='col-span-12 mt-4 md:col-span-4 md:mt-0'>
+            <div className='container rounded-md pb-4 shadow-md shadow-slate-300'>
+              <img
+                src={blogData.image}
+                alt={blogData.title}
+                className='rounded-md p-4 lg:w-full'
+                loading='lazy'
+              />
+              <div className='px-2'>
+                <header className='text-md pt-2 font-semibold text-primary'>
+                  <h5>{blogData?.title}</h5>
                 </header>
-                <section className='pt-4 text-xs font-bold italic text-[#5c5b5b]'>
+                <section className='font-muted pt-4 text-xs font-bold italic text-[#5c5b5b]'>
                   <h6>
                     By BullionMentor on{' '}
                     {new Intl.DateTimeFormat('en-US', {
@@ -45,26 +72,20 @@ const Blog = ({
                     }).format(new Date(blogData.publishdate))}
                   </h6>
                 </section>
-                {/* ----- sub-heading and paragraph ----- */}
-                <Description blogData={blogData} />
-
-                {/*-------------------------- Blog Content End --------------------- */}
-              </span>
-            </div>
-            {/* --------------------- Blog Side Card------------------- */}
-            <div className='col-span-12 mt-4 md:col-span-4 md:mt-0'>
-              <div className='container rounded-md pb-4 shadow-md shadow-slate-300'>
-                <img
-                  src={blogData.image}
-                  alt={blogData.title}
-                  className='rounded-md p-4 lg:w-full'
-                  loading='lazy'
-                />
-                <BlogSideCard blogData={blogData} />
+                <p
+                  className='pt-2 text-justify text-sm leading-[1.4rem] text-[#5c5b5b]'
+                  dangerouslySetInnerHTML={{
+                    __html:
+                      blogData.shortDescription <= 29
+                        ? blogData.shortDescription
+                        : blogData.shortDescription.slice(0, 500) + '...'
+                  }}
+                ></p>
               </div>
             </div>
           </div>
         </div>
+      </div>
     </>
   );
 };
