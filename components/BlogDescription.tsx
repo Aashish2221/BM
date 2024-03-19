93a33aa062370f4f786252d6a143591cb26cdac2
Aashish@2221 <aashishpatil2221@gmail.com>
2024-03-19 15:01:00 +0530

    g

--- components/BlogDescription.tsx
+++ components/BlogDescription.tsx
@@ -8,31 +8,4 @@ export default function Description({ blogData }: any) {
     );
   }
 
- export const BlogSideCard = ({blogData}:any)=>{
-    return(
-      <div className='px-2'>
-        <header className='text-md pt-2 font-semibold text-primary'>
-          <h5>{blogData?.title}</h5>
-        </header>
-        <section className='font-muted pt-4 text-xs font-bold italic text-[#5c5b5b]'>
-          <h6>
-            By BullionMentor on{' '}
-            {new Intl.DateTimeFormat('en-US', {
-              month: 'long',
-              day: 'numeric',
-              year: 'numeric'
-            }).format(new Date(blogData.publishdate))}
-          </h6>
-        </section>
-        <p
-          className='pt-2 text-justify text-sm leading-[1.4rem] text-[#5c5b5b]'
-          dangerouslySetInnerHTML={{
-            __html:
-              blogData.shortDescription <= 29
-                ? blogData.shortDescription
-                : blogData.shortDescription.slice(0, 500) + '...'
-          }}
-        ></p>
-      </div>
-    )
-  }
+ 
