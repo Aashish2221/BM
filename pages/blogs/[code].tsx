947c463f1b08bd4598e5b4c19dfa1992f623766a
Aashish@2221 <aashishpatil2221@gmail.com>
2024-03-20 16:58:35 +0530

    Update [code].tsx

--- pages/blogs/[code].tsx
+++ pages/blogs/[code].tsx
@@ -5,31 +5,16 @@ import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import { getBlogDetails } from '@/services/spot-prices';
 import data from '@/data';
 import BlogSlugSkeleton from '@/components/Loaders/BlogIndexSkeleton/BlogSlugSkeleton';
-import { useState, useEffect } from 'react';
 
 const Blog = ({
   title,
   description,
   blogData
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-  const [showDescription, setShowDescription] = useState(false);
   const router = useRouter();
   const { code } = router.query;
   const formattedPath = router.asPath.replace(`/blogs?.Title=${code}`, '');
   const canonicalUrl = data.WEBSITEUrl + formattedPath;
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const blogDetails = await getBlogDetails(`${code}`);
-        setShowDescription(true);
-      } catch (error) {
-        console.error('Error fetching blog details:', error);
-      }
-    };
-
-    fetchData();
-  }, [code]); // Fetch data whenever the router query parameter 'code' changes
 
   return (
     <>
@@ -42,42 +27,42 @@ const Blog = ({
         )}
       </Head>
       {blogData.length === 0 ? <BlogSlugSkeleton /> :
-      <div className='grid-col container mx-auto grid'>
-        <div className='mx-auto mt-16 grid max-w-[1400px] grid-cols-12 gap-0 text-dark-black sm:container sm:gap-4 md:mt-10'>
-          <div className='col-span-12 md:col-span-8'>
-            <span className='lg:grid-col gap-1 lg:grid'>
-              {blogData.image && (
-                <Image
-                  src={blogData.image}
-                  alt={blogData.title}
-                  height={800}
-                  width={800}
-                  className='rounded-md lg:w-full'
-                  loading='eager'
-                  priority
-                />
-              )}
-              <header className='pt-5 text-lg font-semibold text-primary md:text-2xl md:font-medium'>
-                <h1>{blogData.title}</h1>
-              </header>
-              <section className='pt-4 text-xs font-bold italic text-[#5c5b5b]'>
-                <h6>
-                  By BullionMentor on{' '}
-                  {new Intl.DateTimeFormat('en-US', {
-                    month: 'long',
-                    day: 'numeric',
-                    year: 'numeric'
-                  }).format(new Date(blogData.publishdate))}
-                </h6>
-              </section>
-              {showDescription && <BlogDescription blogData={blogData} />}
-            </span>
-          </div>
-          <div className='col-span-12 mt-4 md:col-span-4 md:mt-0'>
-            <BlogSideCard blogData={blogData} />
+        <div className='grid-col container mx-auto grid'>
+          <div className='mx-auto mt-16 grid max-w-[1400px] grid-cols-12 gap-0 text-dark-black sm:container sm:gap-4 md:mt-10'>
+            <div className='col-span-12 md:col-span-8'>
+              <span className='lg:grid-col gap-1 lg:grid'>
+                {blogData.image && (
+                  <Image
+                    src={blogData.image}
+                    alt={blogData.title}
+                    height={800}
+                    width={800}
+                    className='rounded-md lg:w-full'
+                    loading='eager'
+                    priority
+                  />
+                )}
+                <header className='pt-5 text-lg font-semibold text-primary md:text-2xl md:font-medium'>
+                  <h1>{blogData.title}</h1>
+                </header>
+                <section className='pt-4 text-xs font-bold italic text-[#5c5b5b]'>
+                  <h6>
+                    By BullionMentor on{' '}
+                    {new Intl.DateTimeFormat('en-US', {
+                      month: 'long',
+                      day: 'numeric',
+                      year: 'numeric'
+                    }).format(new Date(blogData.publishdate))}
+                  </h6>
+                </section>
+                  <BlogDescription blogData={blogData}/>
+              </span>
+            </div>
+            <div className='col-span-12 mt-2 md:col-span-4 md:mt-0'>
+              <BlogSideCard blogData={blogData} />
+            </div>
           </div>
         </div>
-      </div>
       }
     </>
   );
@@ -85,16 +70,11 @@ const Blog = ({
 export default Blog;
 
 export const getServerSideProps: GetServerSideProps = async (res) => {
-  try {
-    const code = res.params?.code as string;
-    const blogData = await getBlogDetails(code);
-    const title = blogData.metatitle;
-    const description = blogData.metaDescription;
-    return { props: { title, description, blogData } };
-  } catch (error) {
-    console.error('Error fetching server-side props:', error);
-    return { props: { title: '', description: '', blogData: [] } };
-  }
+  const code = res.params?.code as string;
+  const blogData = await getBlogDetails(code as string);
+  const title = blogData.metatitle;
+  const description = blogData.metaDescription;
+  return { props: { title, description, blogData } };
 };
 
 export const BlogSideCard = ({ blogData }: any) => {
