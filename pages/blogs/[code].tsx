ee3f549b2f3dce03893dad3cd060325635e56dc6
Aashish@2221 <aashishpatil2221@gmail.com>
2024-03-20 10:07:27 +0530

    r

--- pages/blogs/[code].tsx
+++ pages/blogs/[code].tsx
@@ -89,24 +89,10 @@ const Blog = ({
                   <header className='text-md pt-2 font-semibold text-primary'>
                     <h5>{blogData?.title}</h5>
                   </header>
-                  <section className='font-muted pt-4 text-xs font-bold italic text-[#5c5b5b]'>
-                    <h6>
-                      By BullionMentor on{' '}
-                      {new Intl.DateTimeFormat('en-US', {
-                        month: 'long',
-                        day: 'numeric',
-                        year: 'numeric'
-                      }).format(new Date(blogData.publishdate))}
-                    </h6>
-                  </section>
+                  
                   <p
                     className='pt-2 text-justify text-sm leading-[1.4rem] text-[#5c5b5b]'
-                    // dangerouslySetInnerHTML={{
-                    //   __html:
-                    //     blogData && wordCount(blogData?.description) <= 29
-                    //       ? blogData?.description
-                    //       : blogData?.description.slice(0, 500) + '...'
-                    // }}
+                    
                   >{blogData.shortDescription}</p>
                 </div>
               </div>
