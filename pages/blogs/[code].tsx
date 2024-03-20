ad4576367a68a6aba08f7f89eb1517f83ca42957
Aashish@2221 <aashishpatil2221@gmail.com>
2024-03-20 12:52:53 +0530

    Update [code].tsx

--- pages/blogs/[code].tsx
+++ pages/blogs/[code].tsx
@@ -4,60 +4,80 @@ import Head from 'next/head';
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import { getBlogDetails } from '@/services/spot-prices';
 import data from '@/data';
+import { useState, useEffect } from 'react';
+
 const Blog = ({
-  title , description ,
+  title,
+  description,
   blogData
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const router = useRouter();
   const { code } = router.query;
-  const formattedPath = router.asPath.replace(`/blogs?.Title = ${code}`, '');
+  const formattedPath = router.asPath.replace(`/blogs?.Title=${code}`, '');
   const canonicalUrl = data.WEBSITEUrl + formattedPath;
-    return (
-      <>
-        <Head>
-          <title>{title}</title>
-          <meta property='og:url' content={canonicalUrl} key={canonicalUrl} />
-          <link rel='canonical' href={canonicalUrl} />
+  const [showDescription, setShowDescription] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setTimeout(() => {
+        setShowDescription(true);
+      }, 1000);
+    };
+    fetchData();
+  }, []); 
+
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta property='og:url' content={canonicalUrl} key={canonicalUrl} />
+        <link rel='canonical' href={canonicalUrl} />
+        {blogData.image && (
           <link rel="preload" as='image' href={blogData.image} />
-        </Head>
-        <div className='grid-col container mx-auto grid h-full w-full'>
-          <div className='sm:container mx-auto mt-16 grid max-w-[1400px] grid-cols-12 gap-0 text-dark-black sm:gap-4 md:mt-10'>
-            <div className='col-span-12 md:col-span-8'>
-              <span className='lg:grid-col lg:grid gap-1'>
-                  <Image
-                    src={blogData?.image ?? ''}
-                    alt={blogData?.title}
-                    height={800}
-                    width={800}
-                    className='rounded-md lg:w-full'
-                    loading='eager'
-                    priority
-                  />
-                <header className='pt-5 text-lg font-semibold text-primary md:text-2xl md:font-medium'>
-                  <h1>{blogData?.title}</h1>
-                </header>
-                <section className='pt-4 text-xs font-bold italic text-[#5c5b5b]'>
-                  <h6>
-                    By BullionMentor on{' '}
-                    {new Intl.DateTimeFormat('en-US', {
-                      month: 'long',
-                      day: 'numeric',
-                      year: 'numeric'
-                    }).format(new Date(blogData.publishdate))}
-                  </h6>
-                </section>
-               <BlogDescription blogData={blogData} />
-              </span>
-            </div>
-            <div className='col-span-12 mt-4 md:col-span-4 md:mt-0'>
-              <BlogSideCard blogData={blogData}/>
-            </div>
+        )}
+      </Head>
+      <div className='grid-col container mx-auto grid h-full w-full'>
+        <div className='sm:container mx-auto mt-16 grid max-w-[1400px] grid-cols-12 gap-0 text-dark-black sm:gap-4 md:mt-10'>
+          <div className='col-span-12 md:col-span-8'>
+            <span className='lg:grid-col lg:grid gap-1'>
+              {blogData.image && (
+                <Image
+                  src={blogData.image}
+                  alt={blogData.title}
+                  height={800}
+                  width={800}
+                  className='rounded-md lg:w-full'
+                  loading='eager'
+                  priority
+                />
+              )}
+              <header className='pt-5 text-lg font-semibold text-primary md:text-2xl md:font-medium'>
+                <h1>{blogData.title}</h1>
+              </header>
+              <section className='pt-4 text-xs font-bold italic text-[#5c5b5b]'>
+                <h6>
+                  By BullionMentor on{' '}
+                  {new Intl.DateTimeFormat('en-US', {
+                    month: 'long',
+                    day: 'numeric',
+                    year: 'numeric'
+                  }).format(new Date(blogData.publishdate))}
+                </h6>
+              </section>
+              {showDescription && <BlogDescription blogData={blogData} />}
+            </span>
+          </div>
+          <div className='col-span-12 mt-4 md:col-span-4 md:mt-0'>
+            <BlogSideCard blogData={blogData}/>
           </div>
         </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
+}
+
 export default Blog;
+
 
 export const getServerSideProps: GetServerSideProps = async (res) => {
   const code = res.params?.code as string;
