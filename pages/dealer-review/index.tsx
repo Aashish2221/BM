e47dc747102797ac7c205aca341a71b42b9d936e
Aashish@2221 <aashishpatil2221@gmail.com>
2024-03-26 13:18:00 +0530

    Update index.tsx

--- pages/dealer-review/index.tsx
+++ pages/dealer-review/index.tsx
@@ -1,19 +1,16 @@
 import { getDealers } from '@/services/spot-prices';
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
-import Image from 'next/image';
 import useToggle from '@/hooks/useToggle';
 import ReviewModal from '@/components/ModalForm/ReviewModal/ReviewModal';
 import data from '@/data';
+import Image from 'next/image';
 import Head from 'next/head';
 import DealerCard from '@/components/Dealers/dealercard';
 
-export default function DealerReview({title , description ,
-  dealers
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+export default function DealerReview({ title, description, dealers }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const [isOpenModalRegister, toggleModalDealersRating] = useToggle();
   return (
     <>
-      {/* ******************** SEO CONTENT ******************** */}
       <Head>
         <title>{title}</title>
         <meta property='og:type' content={data.OGTags.home.type} />
@@ -26,21 +23,15 @@ export default function DealerReview({title , description ,
           rel='canonical'
           href={`${process.env.WEBSITE_URL}/dealer-review`}
         />
-       
         {
-          dealers.map((dealers:any) =><link key={dealers.id} rel="preload" as='image' href={dealers.image} /> )
+          dealers.map((dealers: any) => <link key={dealers.id} rel="preload" as='image' href={dealers.image} />)
         }
       </Head>
-      {/* ******************** GRADIENT ******************** */}
-      <div className='h-40 bg-gradient-to-b from-secondary via-white to-white'></div>
       <div className='-mt-28 flex flex-col gap-4 text-dark-black'>
-        {/* ******************** HEADING ******************** */}
         <div className='container mx-auto grid grid-cols-4 md:grid-cols-10 md:gap-4 lg:grid-cols-12 '>
-          {/* ******************** HEADING ******************** */}
           <div className='col-span-2 md:col-span-8 md:mt-3 lg:col-span-10 lg:mt-0'>
             <h1 className='text-xl font-medium md:text-2xl'>Dealers</h1>
           </div>
-          {/* ******************** ADD REVIEW BUTTON ******************** */}
           <div className='col-span-2 flex justify-end md:col-span-2 md:mt-3 lg:col-span-2 lg:mt-0'>
             <button
               onClick={toggleModalDealersRating}
@@ -52,14 +43,25 @@ export default function DealerReview({title , description ,
           </div>
         </div>
         <div className='grid-col-4 container mx-auto mt-4 flex flex-col gap-4 md:grid md:grid-cols-8 lg:grid-cols-10 lg:flex-col'>
-          {/* ******************** DEALERS LIST ******************** */}
           <div className='col-span-4 mt-0 md:col-span-5 md:mt-2 lg:col-span-8 lg:mt-0'>
             <div className='grid grid-cols-2 flex-col gap-4 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5'>
-              {dealers.map((dealers:any) => (
+              {dealers.map((dealers: any) => (
                 <div key={dealers.id}>
                   <DealerCard dealers={dealers} />
                 </div>
               ))}
+            </div>
+          </div>
+          <div className=' col-span-4 md:col-span-3 md:ml-4 md:flex lg:col-span-2 lg:ml-4'>
+            <div className='mt-0 w-full  rounded-2xl md:mt-4 md:h-full lg:mt-1 '>
+              <Image
+                src='https://res.cloudinary.com/bullionmentor/image/upload/v1689160172/Infographics/Bullion-Investment-Benefits_ghwffm.webp'
+                alt=''
+                height={1000}
+                width={500}
+                className='rounded-lg'
+                loading='eager'
+              />
             </div>
           </div>
         </div>
@@ -76,6 +78,5 @@ export const getServerSideProps: GetServerSideProps = async ({ res }) => {
   const dealers = await getDealers();
   const title = data.site.dealerslist.page
   const description = data.site.dealerslist.description
-  return { props: {title , description , dealers}
-  };
+  return { props: { title, description, dealers } };
 };
