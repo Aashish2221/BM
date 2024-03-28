4d0c412dc1b5bfed5f113af559df309c4802b63d
Aashish@2221 <aashishpatil2221@gmail.com>
2024-03-28 09:58:59 +0530

    Update index.tsx

--- pages/dealer-review/index.tsx
+++ pages/dealer-review/index.tsx
@@ -1,3 +1,4 @@
+// Import necessary modules and components
 import { getDealers } from '@/services/spot-prices';
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import Image from 'next/image';
@@ -6,44 +7,28 @@ import data from '@/data';
 import Head from 'next/head';
 import dynamic from 'next/dynamic';
 
-const ReviewModal = dynamic(()=>import('@/components/ModalForm/ReviewModal/ReviewModal'))
-const DealerCard = dynamic(()=>import('@/components/Dealers/dealercard'))
+// Dynamic imports for better code splitting
+const ReviewModal = dynamic(() => import('@/components/ModalForm/ReviewModal/ReviewModal'));
+const DealerCard = dynamic(() => import('@/components/Dealers/dealercard'));
 
-export default function DealerReview({title , description ,
-  dealers
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+export default function DealerReview({ title, description, dealers }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const [isOpenModalRegister, toggleModalDealersRating] = useToggle();
+
   return (
     <>
-      {/* ******************** SEO CONTENT ******************** */}
       <Head>
         <title>{title}</title>
         <meta property='og:type' content={data.OGTags.home.type} />
-        <meta
-          property='og:url'
-          content={`${process.env.WEBSITE_URL}/dealer-review`}
-          key={`${process.env.WEBSITE_URL}/dealer-review`}
-        />
-        <link
-          rel='canonical'
-          href={`${process.env.WEBSITE_URL}/dealer-review`}
-        />
-        <link
-          rel='preload'
-          as='image'
-          href='https://res.cloudinary.com/bullionmentor/image/upload/v1689160172/Infographics/Bullion-Investment-Benefits_ghwffm.webp'
-        />
+        <meta property='og:url' content={`${process.env.WEBSITE_URL}/dealer-review`} key={`${process.env.WEBSITE_URL}/dealer-review`} />
+        <link rel='canonical' href={`${process.env.WEBSITE_URL}/dealer-review`} />
+        <link rel='preload' as='image' href='https://res.cloudinary.com/bullionmentor/image/upload/v1689160172/Infographics/Bullion-Investment-Benefits_ghwffm.webp' />
       </Head>
-      {/* ******************** GRADIENT ******************** */}
       <div className='h-40 bg-gradient-to-b from-secondary via-white to-white'></div>
       <div className='-mt-28 flex flex-col gap-4 text-dark-black'>
-        {/* ******************** HEADING ******************** */}
         <div className='container mx-auto grid grid-cols-4 md:grid-cols-10 md:gap-4 lg:grid-cols-12 '>
-          {/* ******************** HEADING ******************** */}
           <div className='col-span-2 md:col-span-8 md:mt-3 lg:col-span-10 lg:mt-0'>
             <h1 className='text-xl font-medium md:text-2xl'>Dealers</h1>
           </div>
-          {/* ******************** ADD REVIEW BUTTON ******************** */}
           <div className='col-span-2 flex justify-end md:col-span-2 md:mt-3 lg:col-span-2 lg:mt-0'>
             <button
               onClick={toggleModalDealersRating}
@@ -55,34 +40,31 @@ export default function DealerReview({title , description ,
           </div>
         </div>
         <div className='grid-col-4 container mx-auto mt-4 flex flex-col gap-4 md:grid md:grid-cols-8 lg:grid-cols-10 lg:flex-col'>
-          {/* ******************** DEALERS LIST ******************** */}
           <div className='col-span-4 mt-0 md:col-span-5 md:mt-2 lg:col-span-8 lg:mt-0'>
             <div className='grid grid-cols-2 flex-col gap-4 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5'>
-              {dealers.map((dealers:any) => (
-                <div key={dealers.id}>
-                <DealerCard dealers={dealers} />
+              {dealers.map((dealer:any) => (
+                <div key={dealer.id}>
+                  <DealerCard dealer={dealer} />
                 </div>
               ))}
             </div>
           </div>
-          {/* ******************** RIGHT ADVERTISEMENTS ******************** */}
           <div className=' col-span-4 hidden md:block md:col-span-3 md:ml-4 lg:col-span-2 lg:ml-4'>
             <div className='mt-0 w-full  rounded-2xl md:mt-4 md:h-full lg:mt-1 '>
               <Image
-              fill
                 src='https://res.cloudinary.com/bullionmentor/image/upload/v1689160172/Infographics/Bullion-Investment-Benefits_ghwffm.webp'
                 alt=''
+                height={1000}
+                width={500}
                 className='rounded-lg'
-                loading='eager'
+                loading='lazy' // Lazy loading
                 priority
               />
             </div>
           </div>
         </div>
       </div>
-      {isOpenModalRegister && (
-        <ReviewModal closeModal={toggleModalDealersRating} />
-      )}
+      {isOpenModalRegister && <ReviewModal closeModal={toggleModalDealersRating} />}
     </>
   );
 }
@@ -90,7 +72,7 @@ export default function DealerReview({title , description ,
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
   res.setHeader('Cache-control', 'public, sa-maxage=10, state-while-revalidate=59');
   const dealers = await getDealers();
-  const title = data.site.dealerslist.page
-  const description = data.site.dealerslist.description
-  return {props: {title , description , dealers}};
+  const title = data.site.dealerslist.page;
+  const description = data.site.dealerslist.description;
+  return { props: { title, description, dealers } };
 };
