c168fee5b2c4ead45f65db28cd89e25f0de1494f
Aashish@2221 <aashishpatil2221@gmail.com>
2024-04-01 17:10:35 +0530

    Update [code].tsx

--- pages/dealer-review/[code].tsx
+++ pages/dealer-review/[code].tsx
@@ -10,38 +10,22 @@ import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import Advertisement from './advertisement';
 import dynamic from 'next/dynamic';
 
-const ReviewModal = dynamic(
-  () => import('@/components/ModalForm/ReviewModal/ReviewModal')
-);
-const VendorReviewModal = dynamic(
-  () => import('@/components/ModalForm/VendorReviewModals/VendorReviewModal')
-);
-const DealerHeader = dynamic(
-  () => import('@/components/Dealers/reviewmobileheader')
-);
-const ReviewDesktopHeader = dynamic(
-  () => import('@/components/Dealers/reviewdesktopheader')
-);
-const DealersReating = dynamic(()=>import('@/components/Dealers/DealersReating'))
-export default function VendorReview({
-  title,
-  description,
-  dealers
+const ReviewModal = dynamic(() => import('@/components/ModalForm/ReviewModal/ReviewModal'));
+const VendorReviewModal = dynamic(() => import('@/components/ModalForm/VendorReviewModals/VendorReviewModal'));
+const DealerHeader = dynamic(() => import('@/components/Dealers/reviewmobileheader'));
+const ReviewDesktopHeader = dynamic(() => import('@/components/Dealers/reviewdesktopheader'));
+const DealersReating = dynamic(() => import('@/components/Dealers/DealersReating'));
+export default function VendorReview({title,description,dealers
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const router = useRouter();
   const { code } = router.query;
   const vendorReviewData = data.site.vendorReview;
   const ogTag = data.OGTags.home;
-  const formattedPath = router.asPath.replace(
-    '/dealers-review?DealerCode=${code}',
-    ''
-  );
+  const formattedPath = router.asPath.replace('/dealers-review?DealerCode=${code}','');
   const canonicalUrl = data.WEBSITEUrl + formattedPath;
   const [isOpenModalRegister, toggleModalDealersRating] = useToggle();
   const [isSuccessModal, toggleSuccessModal] = useToggle();
-
   const [dealer, setDealer] = useState(dealers);
-
   const wordCount = (str: string) => {
     return str.split(' ').length;
   };
@@ -50,14 +34,14 @@ export default function VendorReview({
   const [showMore, setShowMore] = useState(false);
   const [selected, setSelected] = useState(0);
   const handleSelect = (i: number) => {
-  if (showMore && selected === i) {
-    setShowMore(false); // Collapse the content
-    setSelected(-1); // Reset the selected index
-  } else {
-    setShowMore(true); // Expand the content
-    setSelected(i); // Set the selected index
-  }
-}
+    if (showMore && selected === i) {
+      setShowMore(false); // Collapse the content
+      setSelected(-1); // Reset the selected index
+    } else {
+      setShowMore(true); // Expand the content
+      setSelected(i); // Set the selected index
+    }
+  };
 
   return (
     <>
@@ -101,69 +85,71 @@ export default function VendorReview({
                   id='noscroll'
                 >
                   {dealer &&
-                    dealer.dealerReview.slice(0).map((dealers:any, index:any) => (
-                      <div
-                        className={`mt-6 rounded-2xl border-gray-200 bg-white shadow-md shadow-slate-300 md:col-span-6 md:mt-8 md:w-full md:px-6 md:py-4 lg:col-span-2 lg:mt-8
+                    dealer.dealerReview
+                      .slice(0)
+                      .map((dealers: any, index: any) => (
+                        <div
+                          className={`mt-6 rounded-2xl border-gray-200 bg-white shadow-md shadow-slate-300 md:col-span-6 md:mt-8 md:w-full md:px-6 md:py-4 lg:col-span-2 lg:mt-8
                          ${
                            dealer.dealerReview.length < 2
                              ? 'max-h-[25rem] min-h-[15rem] lg:h-min'
                              : 'max-h-[50rem] lg:h-auto'
                          } lg:px-3 lg:py-4`}
-                        key={dealers.id}
-                      >
-                       <DealersReating dealers ={dealers} />
-                        <div className='py-2 px-4 text-sm md:px-6 md:py-4 md:text-base lg:px-6 lg:py-2 lg:text-base'>
-                          <p className='block font-semibold xl:hidden'>
-                            {wordCount(
-                              dealers.reviewHeader ? dealers.reviewHeader : ''
-                            ) <= 6
-                              ? dealers.reviewHeader
-                              : dealers.reviewHeader?.slice(0, 33) + '...'}
-                          </p>
-                          <p className='hidden font-semibold md:hidden md:text-lg lg:text-lg xl:block'>
-                            {dealers.reviewHeader}
-                          </p>
+                          key={dealers.id}
+                        >
+                          <DealersReating dealers={dealers} />
+                          <div className='py-2 px-4 text-sm md:px-6 md:py-4 md:text-base lg:px-6 lg:py-2 lg:text-base'>
+                            <p className='block font-semibold xl:hidden'>
+                              {wordCount(
+                                dealers.reviewHeader ? dealers.reviewHeader : ''
+                              ) <= 6
+                                ? dealers.reviewHeader
+                                : dealers.reviewHeader?.slice(0, 33) + '...'}
+                            </p>
+                            <p className='hidden font-semibold md:hidden md:text-lg lg:text-lg xl:block'>
+                              {dealers.reviewHeader}
+                            </p>
 
-                          <p className='hidden h-24 py-2 md:hidden lg:h-28 xl:h-24'>
-                            {wordCount(
-                              dealers.reviewText ? dealers.reviewText : ''
-                            ) <= 29
-                              ? dealers.reviewText
-                              : dealers.reviewText?.slice(0, 120) + '...'}
-                          </p>
-                          {/* ******************** REVIEW TEXT ******************** */}
-                          <div className='h-auto py-2 text-base text-gray-500'>
-                            {showMore === false && selected === 0
-                              ? dealers.reviewText.slice(0, 120)
-                              : showMore === true && selected === index
-                              ? dealers.reviewText
-                              : dealers.reviewText.slice(0, 120)}
-                            {dealers.reviewText.length > 120 && (
-                              <button
-                                className='text-base font-normal text-primary ml-1'
-                                onClick={() => handleSelect(index)}
-                              >
-                                {showMore === false && selected === 0
-                                  ? 'Read more'
-                                  : showMore === true && selected === index
-                                  ? 'Read less'
-                                  : 'Read more'}
-                              </button>
-                            )}
+                            <p className='hidden h-24 py-2 md:hidden lg:h-28 xl:h-24'>
+                              {wordCount(
+                                dealers.reviewText ? dealers.reviewText : ''
+                              ) <= 29
+                                ? dealers.reviewText
+                                : dealers.reviewText?.slice(0, 120) + '...'}
+                            </p>
+                            {/* ******************** REVIEW TEXT ******************** */}
+                            <div className='h-auto py-2 text-base text-gray-500'>
+                              {showMore === false && selected === 0
+                                ? dealers.reviewText.slice(0, 120)
+                                : showMore === true && selected === index
+                                ? dealers.reviewText
+                                : dealers.reviewText.slice(0, 120)}
+                              {dealers.reviewText.length > 120 && (
+                                <button
+                                  className='ml-1 text-base font-normal text-primary'
+                                  onClick={() => handleSelect(index)}
+                                >
+                                  {showMore === false && selected === 0
+                                    ? 'Read more'
+                                    : showMore === true && selected === index
+                                    ? 'Read less'
+                                    : 'Read more'}
+                                </button>
+                              )}
+                            </div>
+
+                            <p className=' -mb-1 text-base font-light  italic text-slate-600 sm:-mb-4 md:text-base lg:text-base'>
+                              - {dealers.fullName}
+                              ,&nbsp;
+                              {new Intl.DateTimeFormat('en-US', {
+                                month: 'long',
+                                day: 'numeric',
+                                year: 'numeric'
+                              }).format(new Date(dealers.createTS))}
+                            </p>
                           </div>
-
-                          <p className=' text-base -mb-1 sm:-mb-4  font-light italic text-slate-600 md:text-base lg:text-base'>
-                            - {dealers.fullName}
-                            ,&nbsp;
-                            {new Intl.DateTimeFormat('en-US', {
-                              month: 'long',
-                              day: 'numeric',
-                              year: 'numeric'
-                            }).format(new Date(dealers.createTS))}
-                          </p>
                         </div>
-                      </div>
-                    ))}
+                      ))}
                 </div>
               </div>
               {/* ******************** Advertisement ******************** */}
