d2ec094705fed6a70592a7126f4f0bd89c78f1cc
Aashish@2221 <aashishpatil2221@gmail.com>
2024-04-01 18:07:50 +0530

    x

---
+++ components/Dealers/ReaviewRwating.tsx
@@ -0,0 +1,29 @@
+import { TiStarFullOutline } from "react-icons/ti";
+
+const ReviewRating = ({dealers}:any)=>{
+    return(
+        <div className='-sm:mt-5 -mt-1 ml-4 flex items-center md:ml-7 md:flex lg:ml-10 lg:flex'>
+        {Array.from({ length: 5 }, (value, index) => {
+          let numbers = index + 0.5;
+          return (
+            <span key={index}>
+              {dealers.rating >= index + 1 ||
+              dealers.rating >= numbers ? (
+                <TiStarFullOutline
+                  fill='#E49E2F'
+                  className='h-8 w-8 text-yellow-500 md:h-10 md:w-10 lg:h-10 lg:w-10'
+                />
+              ) : (
+                <TiStarFullOutline
+                  fill='#C0C0C0'
+                  className='h-8 w-8 text-yellow-500 md:h-10 md:w-10 lg:h-10 lg:w-10'
+                />
+              )}
+            </span>
+          );
+        })}
+      </div>
+    )
+}
+
+export default ReviewRating;
