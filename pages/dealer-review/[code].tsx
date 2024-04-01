9fa88ca7111a9ce3017832de06cb7d422584740f
Aashish@2221 <aashishpatil2221@gmail.com>
2024-04-01 18:41:24 +0530

    a

--- pages/dealer-review/[code].tsx
+++ pages/dealer-review/[code].tsx
@@ -41,20 +41,7 @@ export default function VendorReview({
   const [isOpenModalRegister, toggleModalDealersRating] = useToggle();
   const [isSuccessModal, toggleSuccessModal] = useToggle();
   const [dealer, setDealer] = useState(dealers);
-  const wordCount = (str: string) => {
-    return str.split(' ').length;
-  };
-  const [showMore, setShowMore] = useState(false);
-  const [selected, setSelected] = useState(0);
-  const handleSelect = (i: number) => {
-    if (showMore && selected === i) {
-      setShowMore(false);
-      setSelected(-1);
-    } else {
-      setShowMore(true);
-      setSelected(i);
-    }
-  };
+ 
   return (
     <>
       <Head>
@@ -92,77 +79,7 @@ export default function VendorReview({
                   </button>
                 </div>
                 {/* ******************** Review list ******************** */}
-                <div
-                  className='mx-auto -mt-2 grid gap-2 overflow-y-scroll py-4 md:grid-cols-6 lg:h-auto lg:grid-cols-4'
-                  id='noscroll'
-                >
-                  {dealer &&
-                    dealer.dealerReview
-                      .slice(0)
-                      .map((dealers: any, index: any) => (
-                        <div
-                          className={`mt-6 rounded-2xl border-gray-200 bg-white shadow-md shadow-slate-300 md:col-span-6 md:mt-8 md:w-full md:px-6 md:py-4 lg:col-span-2 lg:mt-8
-                            ${
-                              dealer.dealerReview.length < 2
-                                ? 'max-h-[25rem] min-h-[15rem] lg:h-min'
-                                : 'max-h-[50rem] lg:h-auto'
-                            } lg:px-3 lg:py-4`}
-                                      key={dealers.id}
-                        >
-                        <ReviewRating dealers={dealer} />
-                          <div className='py-2 px-4 text-sm md:px-6 md:py-4 md:text-base lg:px-6 lg:py-2 lg:text-base'>
-                            <p className='block font-semibold xl:hidden'>
-                              {wordCount(
-                                dealers.reviewHeader ? dealers.reviewHeader : ''
-                              ) <= 6
-                                ? dealers.reviewHeader
-                                : dealers.reviewHeader?.slice(0, 33) + '...'}
-                            </p>
-                            {/* <p className='hidden font-semibold md:hidden md:text-lg lg:text-lg xl:block'>
-                              {dealers.reviewHeader}
-                            </p> */}
-
-                            {/* <p className='hidden h-24 py-2 md:hidden lg:h-28 xl:h-24'>
-                              {wordCount(
-                                dealers.reviewText ? dealers.reviewText : ''
-                              ) <= 29
-                                ? dealers.reviewText
-                                : dealers.reviewText?.slice(0, 120) + '...'}
-                            </p> */}
-                            {/* ******************** REVIEW TEXT ******************** */}
-                            <div className='h-auto py-2 text-base text-gray-500'>
-                              {showMore === false && selected === 0
-                                ? dealers.reviewText.slice(0, 120)
-                                : showMore === true && selected === index
-                                ? dealers.reviewText
-                                : dealers.reviewText.slice(0, 120)}
-                              {dealers.reviewText.length > 120 && (
-                                <button
-                                  className='ml-1 text-base font-normal text-primary'
-                                  onClick={() => handleSelect(index)}
-                                >
-                                  {showMore === false && selected === 0
-                                    ? 'Read more'
-                                    : showMore === true && selected === index
-                                    ? 'Read less'
-                                    : 'Read more'}
-                                </button>
-                              )}
-                            </div>
-
-                            <p className=' -mb-1 text-base font-light  italic text-slate-600 sm:-mb-4 md:text-base lg:text-base'>
-                              - {dealers.fullName}
-                              ,&nbsp;
-                              {new Intl.DateTimeFormat('en-US', {
-                                month: 'long',
-                                day: 'numeric',
-                                year: 'numeric'
-                              }).format(new Date(dealers.createTS))}
-                            </p>
-                          </div>
-                        </div>
-                      ))}
-                </div>
+                <ReviewRating dealer={dealer} />
               </div>
               {/* ******************** Advertisement ******************** */}
               <div className='col-span-6  md:col-span-6 lg:col-span-2 xl:col-span-2'>
