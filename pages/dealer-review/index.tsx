9417ce9f28c4496d2ad092ae512438919f9be507
Aashish@2221 <aashishpatil2221@gmail.com>
2024-04-03 16:25:34 +0530

    Update index.tsx

--- pages/dealer-review/index.tsx
+++ pages/dealer-review/index.tsx
@@ -88,7 +88,7 @@ export default function DealerReview({
             </button>
           </div>
         </div>
-        <div className='grid-col-4 container mx-auto mt-0 flex flex-col gap-4 lg:grid  lg:grid-cols-10 lg:flex-col'>
+        <div className='grid-col-4 container mx-auto -mt-1 flex flex-col gap-4 lg:grid  lg:grid-cols-10 lg:flex-col'>
           {/* ******************** DEALERS LIST ******************** */}
           <div className='col-span-4 -mt-1 md:col-span-5 md:mt-2 lg:col-span-8 lg:mt-0'>
             <InfiniteScroll
