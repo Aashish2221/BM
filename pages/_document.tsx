7d3f072fbff5cc2f5a0fcc6e3cd467a087a5da58
Aashish2221 <135332381+Aashish2221@users.noreply.github.com>
2024-04-11 16:05:03 +0530

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -40,6 +40,14 @@ export default function Document() {
           name='google-signin-client_id'
           content={process.env.GOOGLE_CLIENT_ID}
         />
+         <script
+          async
+          defer
+        >{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5PGLMHN');`}</script>
         <meta
           charSet='utf-8'
           httpEquiv='Content-Security-Policy'
